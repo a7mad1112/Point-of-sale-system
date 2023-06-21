4bfcdb25a1290832306ab56989e0673b93e5b9c5
a7mad1112 <ahmalawneh79@gmail.com>
2023-06-21 23:06:12 +0200

    hero section in products page

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -3,7 +3,10 @@ import useFetch from "../../hooks/useFetch";
 import { useDispatch } from "react-redux";
 import Loader from "../Components/Loader/Loader";
 import { productsActions } from "../../store/states/productsSlice";
-
+import SectionHeading from "../Components/SectionHeading/SectionHeading";
+import { Box } from "@mui/material";
+import { Button } from "@mui/material";
+import productsImg from "../../assets/products-img.png";
 function Products() {
   const { response } = useFetch(
     "http://localhost:1337/api/products1?populate=*"
@@ -22,9 +25,48 @@ function Products() {
     );
 
   return (
-    <>
-      <section></section>
-    </>
+    <section>
+      <SectionHeading position="left" text="Products" />
+      <Box
+        display="flex"
+        flexDirection="row"
+        flexWrap="wrap"
+        justifyContent="space-between"
+        alignItems="flex-start"
+        gap={2}
+      >
+        <Box flex={1} minWidth={300} margin="auto" p={2}>
+          <p>
+            Effortlessly manage your product inventory on the Products Page.
+            View, update, and delete existing products to keep your inventory
+            organized and accurate. Take control of your inventory before
+            creating new products.
+          </p>
+          <Button
+            // onClick={() => setShowCreateModal(true)}
+            sx={{
+              my: 4,
+              backgroundColor: "var(--yellow-color)",
+              color: "#FFFFFF",
+              fontWeight: "bold",
+              transition: "0.3s",
+              "&:hover": {
+                opacity: 0.8,
+                backgroundColor: "var(--yellow-color)",
+                transform: "translateY(-2px)",
+              },
+            }}
+            variant="contained"
+          >
+            Create Category
+          </Button>
+        </Box>
+
+        <Box flex={2} minWidth={300} maxWidth={400} p={2} margin="auto">
+          <img src={productsImg} alt="Products" />
+        </Box>
+      </Box>
+    </section>
   );
 }
 
